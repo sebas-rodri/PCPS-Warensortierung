6b749b3046e1d0af740101a658a8511354062a26
Sebastian Rodriguez <sebastianrodriguez@sebastians-mbp.speedport.ip>
2024-05-19 22:03:31 +0200

    Implemented handleCommand, and start server
    Signed-off-by: Sebastian Rodriguez <sebastianrodriguez@sebastians-mbp.speedport.ip>

---
+++ handleRequest.py
@@ -0,0 +1,56 @@
+import socket 
+from arduino_commands import *
+host = '0.0.0.0'
+port = 12345
+
+
+def handleCommand(command) -> None:
+    """
+    Handles the given command.
+
+    Parameters:
+    command (int): The command to be handled.
+
+    Returns:
+    None
+
+    Raises:
+    None
+    """
+    command = int(command) # Convert the string to an integer
+    if command == START:
+        print("Start")
+    elif command == RESET:
+        print("Reset")
+    elif command == BUCKET_ONE:
+        print("Bucket one")
+    elif command == BUCKET_TWO:
+        print("Bucket two")
+    else:
+        print("Invalid command")
+
+
+def startServer() -> None:
+    """
+    Starts the server and listens for incoming connections.
+    """
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port)) # Bind to the port
+    server_socket.listen(5)
+    print(f"Server listening on {host}:{port}")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
+
+        try:
+            command = client_socket.recv(1) # Receive 1 byte, bcs we know the command is 1 byte
+            if command:
+                handleCommand(command.decode("utf-8")) # Decode the byte string
+            client_socket.close()
+        except Exception as e:
+            print(f"Error: {e}")
+            client_socket.close()
+
+if __name__ == "__main__":
+    startServer()

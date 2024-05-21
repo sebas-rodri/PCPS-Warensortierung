94ed1c1563208a596ca542f2956145b9c4baae7b
DerBerliner <melodie-yogi-0l@icloud.com>
2024-05-21 14:02:15 +0200

    add tutorial from git Readme and translate to eng

--- raspi/main.py
+++ raspi/main.py
@@ -1,2 +1,26 @@
-import wlkata_mirobot
+'''
+Position control, point to point (P2P)
+'''
+import time
+from wlkata_mirobot import WlkataMirobot
 
+# create instance of microbot
+arm = WlkataMirobot()
+# Return to mechanical zero Homing (synchronous mode)
+arm.home()
+
+print("Move to target point A")
+arm.set_tool_pose(200,  20, 230)
+print(f"Current position of robot -> {arm.pose}")
+time.sleep(1)
+
+print("Move to target point B")
+arm.set_tool_pose(200,  20, 150)
+print(f"Current position of robot -> {arm.pose}")
+time.sleep(1)
+
+print("Move to target point C with specified angle")
+arm.set_tool_pose(150,  -20,  230, roll=30.0, pitch=0, yaw=45.0)
+print(f"Current position of robot -> {arm.pose}")
+
+arm.home()

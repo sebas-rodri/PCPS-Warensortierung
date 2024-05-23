7e9722d6025b0974ad51620d60a7d2f7ea8f5fba
DerBerliner <melodie-yogi-0l@icloud.com>
2024-05-23 13:31:22 +0200

    extract positions of scale, box1, box2

--- raspi/robot_functions.py
+++ raspi/robot_functions.py
@@ -1,6 +1,10 @@
 from wlkata_mirobot import WlkataMirobot
 import time
 
+POSITION_SCALE = (200, 20, 230)
+POSITION_BOX_1 = (150, -30, 130)
+POSITION_BOX_2 = (150, 30, 130)
+
 
 class Robot(WlkataMirobot):
     def __init__(self, *device_args, **device_kwargs):
@@ -24,14 +28,14 @@ class Robot(WlkataMirobot):
     def itemToBoxOne(self):
         """
         Picks up item from scale and moves item to box one.
-        :return: 1
+        :return: 1 if successful
         """
         # Move to scale
-        self.set_tool_pose(200, 20, 230)
+        self.set_tool_pose(POSITION_SCALE[0], POSITION_SCALE[1], POSITION_SCALE[2])
         # Pickup item
         self.pump_suction()
         # Move to box 1
-        self.set_tool_pose(-200, 10, 230)
+        self.set_tool_pose(POSITION_BOX_1[0], POSITION_BOX_1[1], POSITION_BOX_1[2])
         # Release item
         self.pump_blowing()
         time.sleep(1)
@@ -43,14 +47,14 @@ class Robot(WlkataMirobot):
     def itemToBoxTwo(self):
         """
         Picks up item from scale and moves item to box two.
-        :return:
+        :return: 1 if successful
         """
         # Move to scale
-        self.set_tool_pose(200, 20, 230)
+        self.set_tool_pose(POSITION_SCALE[0], POSITION_SCALE[1], POSITION_SCALE[2])
         # Pickup item
         self.pump_suction()
         # Move to box 2
-        self.set_tool_pose(-200, 30, 230)
+        self.set_tool_pose(POSITION_BOX_2[0], POSITION_BOX_2[1], POSITION_BOX_2[2])
         # Release item
         self.pump_blowing()
         time.sleep(1)

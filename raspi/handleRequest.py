7a111f361b228f03810ccf2b6274f3f6876efb73
erikh <erik.haarlaender@outlook.de>
2024-05-26 22:42:59 +0200

    reformat handleRequest.py to fit standards Reviewed-by: Erik Haarländer <erik.haarlaender@campus.tu-berlin.de>

--- raspi/handleRequest.py
+++ raspi/handleRequest.py
@@ -1,5 +1,6 @@
-import socket 
+import socket
 from arduino_commands import *
+
 host = '0.0.0.0'
 port = 2360
 
@@ -17,7 +18,7 @@ def handleCommand(command) -> None:
     Raises:
     None
     """
-    command = int(command) # Convert the string to an integer
+    command = int(command)  # Convert the string to an integer
     if command == RESET:
         print("Reset")
     elif command == BUCKET_ONE:
@@ -28,7 +29,7 @@ def handleCommand(command) -> None:
         print("Invalid command")
 
 
-def get_local_ip():
+def getLocalIP():
     try:
         # Create a socket object
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -43,23 +44,25 @@ def get_local_ip():
         print("Error:", e)
         return None
 
+
 def startServer() -> None:
     """
     Starts the server and listens for incoming connections.
     """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((host, port)) # Bind to the port
+    server_socket.bind((host, port))  # Bind to the port
     print(f"Server listening on {host}:{port}")
 
     while True:
         try:
-            command, client_address = server_socket.recvfrom(1) # Receive 1 byte, bcs we know the command is 1 byte
+            command, client_address = server_socket.recvfrom(1)  # Receive 1 byte, bcs we know the command is 1 byte
             if command:
                 print(client_address)
-                handleCommand(command[0]) # Decode the byte string
+                handleCommand(command[0])  # Decode the byte string
         except Exception as e:
             print(f"Error: {e}")
 
+
 if __name__ == "__main__":
-    print(get_local_ip())
+    print(getLocalIP())
     startServer()

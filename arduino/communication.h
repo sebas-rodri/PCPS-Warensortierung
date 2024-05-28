0d73e318b4a8dabb630480be4ca37cbe17cb8bbc
cyberspace <antleo2002@gmail.com>
2024-05-28 18:01:37 +0200

    hab die Innitialisirung der LED sowie der Lichtschranke hinzugefügt außerdem wurde die Funktion Fotoelectriksensor implementiert welche mit Testlichschranke getestet werden kann

--- arduino/communication.h
+++ arduino/communication.h
@@ -1,25 +1,19 @@
+#define SECRET_SSID "planthubwifi"
+#define SECRET_PASS "WdBKADGBDRxefs6"
+
 /*
 *Communication interface which defines messages for Arduino Wifi Connectivity
 *Should be updated with corresponding Python File
 *Author: Sebastian Rodriguez
 */
-
-#define RESET		0
-#define BUCKET_ONE	1
-#define BUCKET_TWO	2
-
-/*CONFIGURE*/
-#define SECRET_SSID "Totogolazo" //Config
-#define SECRET_PASS ""        //Depends on Router
-#define PORT        2360      //Make sure to adress the same Port on Arduino and Python
-#define IP_ADDRESS  "0.0.0.0" //Get from Handlerequest.py getLocalIp()
-/*------------------------------------------------------------*/
+#define RESET        (byte)0
+#define BUCKET_ONE    (byte)1
+#define BUCKET_TWO    (byte)2
+#define PORT        2360
+#define IP_ADDRESS  "192.168.1.107"
 
 
 
 int setUpWiFi();
-
 int sendPacket(byte command);
-
 int recvPacket();
-
--- arduino/warensortierung.ino
+++ arduino/warensortierung.ino
@@ -1,16 +1,30 @@
 #include <WiFiS3.h>
 #include "communication.h"
+#include <SoftwareSerial.h>
+#include "Ultrasonic.h"
+
+/* Definition for sensor connections slots */
+#define Lichtschranke A0
+#define LED 10
+#define Laser 2
+// Sets the differenz in Voltag for the lightbarrier to trigger
+#define sensitivity_lightbarrier -100
+
+
+
+
 
 const float MAX_WEIGHT = 500;
 
 unsigned int NR_BOXES;
 float THRESHOLD;
 int *boxes_array;
-
+//Variable for the stadart value of the lightresistir
+int standart_lichtwiederstand = 0;
 
 /*-----WIFI STUFF--------*/
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;
 WiFiUDP Udp;
 
@@ -21,35 +35,55 @@ WiFiUDP Udp;
  */
 // to implement
 void exitFunction() {
-
 }
 
 /**
  * initializing an array of boxes where the number of packages per box are counted.
  */
 void initializingArray() {
-    // declaration
-    boxes_array = (unsigned int*)malloc(NR_BOXES * sizeof(int));
-    if (boxes_array == NULL) {
-        // error handling
-        exitFunction();
-    }
+  // declaration
+  boxes_array = (int *)malloc(NR_BOXES * sizeof(int));
+  if (boxes_array == NULL) {
+    // error handling
+    exitFunction();
+  }
 
-    // initialization with 0
-    for (int i = 0; i < NR_BOXES; i++) { boxes_array[i] = 0; }
-    return;
+  // initialization with 0
+  for (int i = 0; i < NR_BOXES; i++) { boxes_array[i] = 0; }
+  return;
 }
 
 // setup code to run once:
 void setup() {
-    //NR_BOXES initialisieren
-    //THRESHOLD initialisieren
+  // Setup for testing with Serialport(9600)
+  Serial.begin(9600);
+  Serial.print("Testbegin\n");
 
-    initializingArray();
+  /* Initialsierung der Sensoren*/
+  pinMode(LED, OUTPUT);
+  pinMode(Laser, OUTPUT);
+  pinMode(Lichtschranke, INPUT);
+  standart_lichtwiederstand = analogRead(Lichtschranke);
+  Serial.println(standart_lichtwiederstand);
 
-    // Waage
-    // Lichtschranke
-    // LED
+  digitalWrite(Laser, HIGH);
+
+  //visul output for Startup
+  digitalWrite(LED, HIGH);
+  delay(5000);
+  digitalWrite(LED, LOW);
+
+  //NR_BOXES initialisieren
+  //THRESHOLD initialisieren
+
+  initializingArray();
+}
+
+int test_light_barrier(){
+  for (int i = 0; i <= 69; i++) {
+    Serial.println(photoelectricSensor());
+    delay(5000);
+  }
 }
 
 /**
@@ -59,7 +93,6 @@ void setup() {
  */
 // to implement
 void sendMessage(String message) {
-
 }
 
 /**
@@ -69,7 +102,6 @@ void sendMessage(String message) {
  */
 // to implement
 float scale() {
-
 }
 
 /**
@@ -79,9 +111,9 @@ float scale() {
  * @returns number of box or -1 on error
  */
 int sort(float weight) {
-    if (weight < 0 || weight > THRESHOLD) { return -1; }    // Fehlerbehandlung
-    if (weight < THESHOLD) { return 0; }                    // Box 0
-    return 1;                                               // Box 1
+  if (weight < 0 || weight > THRESHOLD) { return -1; }  // Fehlerbehandlung
+  if (weight < THRESHOLD) { return 0; }                 // Box 0
+  return 1;                                             // Box 1
 }
 
 /**
@@ -90,103 +122,106 @@ int sort(float weight) {
  * @return number of full box or -1 if not triggered
  */
 int photoelectricSensor() {
-
+  if ((standart_lichtwiederstand - analogRead(Lichtschranke)) < sensitivity_lightbarrier) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 // main code to run repeatedly:
 void loop() {
-    float weight = scale();
-    int witch_box = sort(weight);
+  float weight = scale();
+  int witch_box = sort(weight);
 
-    String messageWB = String(witch_box);         // convert int to String: message to be send to raspberry
-    sendMessage(messageWB);
+  String messageWB = String(witch_box);  // convert int to String: message to be send to raspberry
+  sendMessage(messageWB);
 
-    boxes_array[witch_box]++;                     // increase the counter for the amount of packages in the box
+  boxes_array[witch_box]++;  // increase the counter for the amount of packages in the box
 
-    // warten auf message das roboter ferig ist
+  // warten auf message das roboter ferig ist
 
-    int full_box = photoelectricSensor();
-    // if a box is full send the relevant info to raspberry and terminate program
-    if (full_box >= 0) {
-        String messageFB = String(full_box);
-        sendMessage(messageFB);
-        exitFunction();
-    }
-
+  int full_box = photoelectricSensor();
+  // if a box is full send the relevant info to raspberry and terminate program
+  if (full_box >= 0) {
+    String messageFB = String(full_box);
+    sendMessage(messageFB);
+    exitFunction();
+  }
 }
 
 /**
 *Set up for the WIFI uses predefined SSID and Password
 */
 int setUpWiFi() {
-    Serial.begin(9600);
-    while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB port only
-    }
+  Serial.begin(9600);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
 
-    // check for the WiFi module:
-    if (WiFi.status() == WL_NO_MODULE) {
-        Serial.println("Communication with WiFi module failed!");
-        // don't continue
-        while (true);
-    }
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true)
+      ;
+  }
 
-    String fv = WiFi.firmwareVersion();
-    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-        Serial.println("Please upgrade the firmware");
-    }
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
 
-    // attempt to connect to WiFi network:
-    while (status != WL_CONNECTED) {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(ssid);
-        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-        status = WiFi.begin(ssid, pass);
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
 
-        // wait 10 seconds for connection:
-        delay(10000);
-    }
-    Serial.println("Connected to WiFi");
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
 
-    // print your board's IP address:
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 
-    // print the received signal strength:
-    long rssi = WiFi.RSSI();
-    Serial.print("signal strength (RSSI):");
-    Serial.print(rssi);
-    Serial.println(" dBm");
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 
-    Serial.println("\nStarting connection to server...");
-    // if you get a connection, report back via serial:
-    Udp.begin(PORT);
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  Udp.begin(PORT);
 }
 
 /**
 *Sends a UDP Packet with the give command
 */
 int sendPacket(byte command) {
-    Serial.println("Send Packet");
-    if (!Udp.beginPacket(IPAddress(IP_ADDRESS), PORT)) {
-        Serial.println("Problem Udp.beginPacket");
-    }
+  Serial.println("Send Packet");
+  if (!Udp.beginPacket(IPAddress(IP_ADDRESS), PORT)) {
+    Serial.println("Problem Udp.beginPacket");
+  }
 
-    Udp.write(command);
+  Udp.write(command);
 
 
-    if (!Udp.endPacket()) {
-        Serial.println("The packet wasnt send.");
-    }
-    Serial.println(command);
+  if (!Udp.endPacket()) {
+    Serial.println("The packet wasnt send.");
+  }
+  Serial.println(command);
 }
 
 /**
 *Recv UDP Packet from Raspberry Pi (TODO)
 */
 int recvPacket() {
-
 }

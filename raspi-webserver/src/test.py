130438018f1d6585401f7f97b2e83edb9a64166a
Sebastian Rodriguez <r99@melao.de>
2024-06-22 20:01:36 +0200

    Implemented Session Class
    Signed-off-by: Sebastian Rodriguez <r99@melao.de>

--- raspi-webserver/src/test.py
+++ raspi-webserver/src/test.py
@@ -5,6 +5,44 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
+class Session:
+    def __init__(self):
+        self.active = True
+        self.box1 = 0
+        self.box2 = 0
+    
+    def start_pause(self):
+        self.active = not self.active
+        print(f'Active: {self.active}')
+    
+    def update_box1(self, value):
+        self.box1 = value
+        print(f'Box1: {self.box1}')
+    
+    def update_box2(self, value):
+        self.box2 = value
+        print(f'Box2: {self.box2}')
+    
+    def increment_box1(self):
+        self.box1 += 1
+        print(f'Box1: {self.box1}')
+    
+    def increment_box2(self):
+        self.box2 += 1
+        print(f'Box2: {self.box2}')
+    
+    def decrement_box1(self):
+        if self.box1 > 0:
+            self.box1 -= 1
+        print(f'Box1: {self.box1}')
+    
+    def decrement_box2(self):
+        if self.box2 > 0:
+            self.box2 -= 1
+        print(f'Box2: {self.box2}')
+    
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,5 +61,10 @@ def handle_message(data):
     # Process the message and optionally emit back to the client
     emit('message_from_server', {'response': 'Message received'})
 
+@socketio.on('start/pause')
+def handle_start_pause():
+    session.start_pause()
+
 if __name__ == '__main__':
+    session = Session()
     socketio.run(app, debug=True)

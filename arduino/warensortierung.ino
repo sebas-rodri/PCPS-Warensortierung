e8cf538aae5b34c3e69ffcd7bdc99b3b9406165f
Eleni <e.kiometzis@campus.tu-berlin.de>
2024-06-25 17:35:15 +0200

    testTCP in warensortierung eingebettet und communication.h leicht angepasst

--- arduino/warensortierung.ino
+++ arduino/warensortierung.ino
@@ -7,70 +7,121 @@
 #if defined(ESP8266) || defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
-/* Definition for sensor connections slots */
-#define LIGHT_BARRIER A0
+
+/*---- Definition for sensor connections slots ----*/
+#define LIGHT_BARRIER A4
 #define LED 10
 #define LASER 2
-#define BUTTON 3
-// Sets the difference in voltage for the light barrier to trigger
-#define SENSITIVITY_LIGHT_BARRIER -100
-// define pins for scale and initializes LoadCell
-const int HX711_dout = 4;  // mcu > HX711 dout pin
-const int HX711_sck = 5;   // mcu > HX711 sck pin
+#define BUTTON 7
+#define SENSITIVITY_LIGHT_BARRIER -100      // Sets the difference in voltage for the light barrier to trigger
+
+/*---- define pins for scale and initializes LoadCell ----*/
+const int HX711_dout = 6;  // mcu > HX711 dout pin
+const int HX711_sck = 7;   // mcu > HX711 sck pin
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 const int calVal_eeprom_address = 0;
 unsigned long t = 0;
 
-// initialize global variables
+/*---- initialize global variables ----*/
+const float MAX_WEIGHT = 10000;     // maximal weight for the packages
+float THRESHOLD = 20;               // weight threshold for package sorting
+unsigned int NR_BOXES = 2;          // number of boxes
+int standard_lb = 60;               // Variable for the standard value of the light barrier
+int full_box = -1;                  // variable for the box status
 
-//maximal weight for the packages
-const float MAX_WEIGHT = 500;
-// weight threshold for package sorting
-float THRESHOLD;
-// number of boxes
-unsigned int NR_BOXES = 1;
-// array for storing the amount of packages
-int *boxes_array;
-//Variable for the standard value of the light barrier
-int standard_lb = 0;
-//variable for the box status
-int full_box = -1;
+/*---- Initializing Variable for WIFI ----*/
+char ssid[] = SECRET_SSID;                        // your network SSID (name), see communication.h
+char pass[] = SECRET_PASS;                        // your network password (use for WPA, or use as key for WEP), see communication.h
+const char* TCP_SERVER_ADDR = IP_ADDRESS;         // see communication.h
+const int TCP_SERVER_PORT = PORT;                 // see communication.h
 
-/*-----Initializing Variable for WIFI--------*/
-char ssid[] = SECRET_SSID;  // your network SSID (name)
-char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;
-WiFiUDP Udp;
+WiFiClient TCP_client;
 
-/*-----------------------*/
+/*____________________ Set-Up Functions ____________________*/
 
 /**
- * orderly shut down of the entire system
+ * orderly shut down of the entire system and sending of error messages (only after wifi connection is set up)
  */
-// to implement
-void exitFunction() {
+void exitFunction(char error) {
+    // send error message to raspi
+    char* message = assembleData(error, 0.0);
+    sendData(message);
+    TCP_client.stop();
+    exit(0);
 }
 
+
 /**
- * initializing an array of boxes where the number of packages per box are counted.
+ * Set up for the WIFI uses predefined SSID and Password
  */
-void initializingArray() {
-    // declaration
-    boxes_array = (int *) malloc(NR_BOXES * sizeof(int));
-    // error handling
-    if (boxes_array == NULL) {
-        exitFunction();
+int setUpWiFi() {
+    Serial.println("Arduino: TCP CLIENT");
+
+    // check for the WiFi module:
+    if (WiFi.status() == WL_NO_MODULE) { 
+      Serial.println("Communication with WiFi module failed!"); 
+      exit(0);                    // on error terminate program
     }
 
-    // initialization with 0
-    for (int i = 0; i < NR_BOXES; i++) { boxes_array[i] = 0; }
-    return;
+    /*String fv = WiFi.firmwareVersion();
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+    }*/
+
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // attempt to connect to WiFi network:
+    while (WiFi.begin(ssid) != WL_CONNECTED) {
+        delay(10000);  // wait 10 seconds for connection
+    }
+
+    Serial.print("Connected to WiFi ");
+    Serial.println(ssid);
+
+    // connect to TCP server
+    if (TCP_client.connect(TCP_SERVER_ADDR, TCP_SERVER_PORT)) {
+        Serial.println("Connected to TCP server");
+        TCP_client.write("Hello!");  // send to TCP Server
+        TCP_client.flush();
+    }
+    else {
+        Serial.println("Failed to connect to TCP server");
+    }
 }
 
-// setup code to run once:
+
+/**
+ * setup the scale for measurements
+ */
+void startup_Scale() {
+    LoadCell.begin();
+    //LoadCell.setReverseOutput();                          // uncomment to turn a negative output value to positive
+    float calibration_value;                                // calibration value (see example file "Calibration.ino")
+    calibration_value = 800;                                // uncomment to set the calibration value in the sketch
+
+    //#if defined(ESP8266) || defined(ESP32)
+    //    EEPROM.begin(512);                                      // uncomment this to use ESP8266/ESP32 and fetch the calibration value from eeprom
+    //#endif
+    //    EEPROM.get(calVal_eeprom_address, calibration_value);   // fetch the calibration value from eeprom
+
+    unsigned long stabilizing_time = 2000;                  // precision right after power-up can be improved by adding a few seconds of stabilizing time
+    boolean _tare = true;                                   // set false to skip tare in the next step
+    LoadCell.start(stabilizing_time, _tare);
+    if (LoadCell.getTareTimeoutFlag()) {
+        exitFunction('s');
+    } 
+    else {
+        LoadCell.setCalFactor(calibration_value);           // set calibration value (float)
+        Serial.println("Startup is complete");
+    }
+}
+
+
+/**
+ * setup code to run once:
+ */
 void setup() {
-    // Setup for testing with serial port(9600)
-    Serial.begin(9600);
+    Serial.begin(9600);                         // Setup for testing with serial port(9600)
     Serial.print("test begin\n");
 
     /* initialize the sensors*/
@@ -81,175 +132,134 @@ void setup() {
     standard_lb = analogRead(LIGHT_BARRIER);
     digitalWrite(LASER, HIGH);
 
-    // start up scale
+    /* start further necesseties */
+    setUpWiFi();
     startup_Scale();
+    // init THRESHOLD with data received from raspi
 
-    // init NR_BOXES
-    // init THRESHOLD
-    initializingArray();
-
-    //visual output for Startup
+    /* visual output for Startup */
     digitalWrite(LED, HIGH);
     delay(5000);
     digitalWrite(LED, LOW);
 }
 
 
-/**
- * setup the scale for measurements
- * 
- * @param none is the message to send
- */
-void startup_Scale() {
-    LoadCell.begin();
-    //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-    float calibration_value;  // calibration value (see example file "Calibration.ino")
-    //calibration_value = 887.24; // uncomment to set the calibration value in the sketch
-#if defined(ESP8266) || defined(ESP32)
-    EEPROM.begin(512);  // uncomment this to use ESP8266/ESP32 and fetch the calibration value from eeprom
-#endif
-    EEPROM.get(calVal_eeprom_address, calibration_value);  // fetch the calibration value from eeprom
-    unsigned long stabilizing_time = 2000;               // precision right after power-up can be improved by adding a few seconds of stabilizing time
-    boolean _tare = true;                               // set false to skip tare in the next step
-    LoadCell.start(stabilizing_time, _tare);
-    if (LoadCell.getTareTimeoutFlag()) {
-        Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-        while (1);
-    } else {
-        LoadCell.setCalFactor(calibration_value);  // set calibration value (float)
-        Serial.println("Startup is complete");
-    }
-
-}
-
+/*____________________ LOOP Functions ____________________*/
 
 /**
- * read out the scale and handle errors
+ * read out the scale
  * 
  * @returns weight of package
  */
-// to implement
 float scale() {
-    while (!LoadCell.update()) {}  // wait for scale output
+    while (!LoadCell.update()) {}           // wait for scale output
     float weight = LoadCell.getData();
-    //Serial.println(weight); // test scale weight
+    //Serial.println(weight);               // test scale weight
     return weight;
 }
 
-/**
- * sorting the package into the right box (Box 0 with least weight)
- * 
- * @param weight is the weight of the package to be sorted
- * @returns number of box or -1 on error
- */
-int sort(float weight) {
-    if (weight < 0 || weight > THRESHOLD) { exitFunction(); }  // error handling
-    if (weight < THRESHOLD) { return 0; }                      // Box 0
-    return 1;                                                  // Box 1
-}
 
 /**
  * read out the photoelectric sensor
  * 
- * @return -1 if triggered and 1 if not triggered
+ * @return -1 if triggered and 0 if not triggered (???)
+
+ Direct exitFinction if barrier is triggered???
  */
 int light_barrier() {
     if ((standard_lb - analogRead(LIGHT_BARRIER)) < SENSITIVITY_LIGHT_BARRIER) {
         return -1;
-    } else {
-        return 0;
     }
+    return 0;
 }
 
-// main code to run repeatedly:
+
+/**
+ * sorting the package into the right box (Box 1 with least weight)
+ * 
+ * @returns number of box (1 or 2) or terminates program with error
+ */
+char sorting() {
+    // ???
+    for (int i = 0; i <= 20; i++) {
+        Serial.println(scale());
+        delay(50);
+    }
+
+    float weight = scale();   // figure out weight
+    
+    /* actual sorting */
+    if (weight < 0 || weight > MAX_WEIGHT) { exitFunction('w'); }   // error handling
+    if (weight < THRESHOLD) { return '1'; }                         // Box 1
+    return '2';                                                     // Box 2
+}
+
+/**
+ * assemble data to be send to raspberry pi into string according to specifications
+ *
+ * @return message to be send as string
+ */
+char* assembleData(char message, float weight) {
+    return "no message";
+}
+
+
+/**
+ * send Data over TCP connection tp Raspberry Pi
+ * @param message is the message to send
+ */
+void sendData(char* message) {
+    if (!TCP_client.connected()) {
+        Serial.println("Connection is disconnected");
+        TCP_client.stop();
+
+        // reconnect to TCP server
+        if (TCP_client.connect(TCP_SERVER_ADDR, TCP_SERVER_PORT)) {
+            Serial.println("Reconnected to TCP server");
+            TCP_client.write("Hello!");  // send to TCP Server
+            TCP_client.flush();
+        } else {
+            Serial.println("Failed to reconnect to TCP server");
+            delay(1000);
+        }
+    }
+
+    TCP_client.write(message);  // send to TCP Server
+    TCP_client.flush();
+}
+
+
+/**
+ * main code to run repeatedly:
+ */
 void loop() {
+    if (1 == 1) {
 
-    // checks the light barrier and exits the function if triggered
-    if (light_barrier() < 0) {
-        exitFunction();
-    }
-    // sorting package and sending instructions to robot
-    sendPacket(byte(sorting()));
-}
+        // checks the light barrier and exits the function if triggered
+        if (light_barrier() == -1) {
 
+            //Serial.println(sorting());
+            char* message = assembleData(sorting(), scale());   // assemble string to send to raspberry pi
+            sendData(message);
+            digitalWrite(LED, HIGH);                  // signal for sending
 
-/**
-* function sorting the package and updating package count
-* return 1 = box_1 return 2 = box_2
-*/
-int sorting() {
-    float weight = scale();
-    int witch_box = sort(weight);
-    boxes_array[witch_box]++;  // increase the counter for the amount of packages in the box
-    return witch_box;
-}
+            delay(10000);
+            digitalWrite(LED, LOW);
 
-
-/**
-* Set up for the WIFI uses predefined SSID and Password
-*/
-int setUpWiFi() {
-    Serial.begin(9600);
-    while (!Serial) { ;  // wait for serial port to connect. Needed for native USB port only
-    }
-
-    // check for the Wi-Fi module:
-    if (WiFi.status() == WL_NO_MODULE) {
-        Serial.println("Communication with WiFi module failed!");
-        // don't continue
-        while (true);
+        }
+        // error handeling noch ausarbeten
+        else {
+            Serial.println("Lichtschranke blockiert");    //error message
+            digitalWrite(LED, HIGH);
+            delay(200);
+            digitalWrite(LED, LOW);
+            digitalWrite(LED, HIGH);
+            delay(200);
+            digitalWrite(LED, LOW);
+            digitalWrite(LED, HIGH);
+            delay(200);
+            digitalWrite(LED, LOW);
+        }
     }
 
-    String fv = WiFi.firmwareVersion();
-    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-        Serial.println("Please upgrade the firmware");
-    }
-
-    // attempt to connect to Wi-Fi network:
-    while (status != WL_CONNECTED) {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(ssid);
-        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-        status = WiFi.begin(ssid, pass);
-
-        // wait 10 seconds for connection:
-        delay(10000);
-    }
-    Serial.println("Connected to WiFi");
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());
-
-    // print your board's IP address:
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
-
-    // print the received signal strength:
-    long rssi = WiFi.RSSI();
-    Serial.print("signal strength (RSSI):");
-    Serial.print(rssi);
-    Serial.println(" dBm");
-
-    Serial.println("\nStarting connection to server...");
-    // if you get a connection, report back via serial:
-    Udp.begin(PORT);
-}
-
-/**
-* @param command is the parameter to sends a UDP Packet with the give command
-*/
-
-int sendPacket(byte command) {
-    Serial.println("Send Packet");
-    if (!Udp.beginPacket(IPAddress(IP_ADDRESS), PORT)) {
-        Serial.println("Problem Udp.beginPacket");
-    }
-
-    Udp.write(command);
-
-
-    if (!Udp.endPacket()) {
-        Serial.println("The packet wasn't send.");
-    }
-    Serial.println(command);
 }

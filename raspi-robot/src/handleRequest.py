9b67f267d39e90b4632be4e0b6589d337ba71355
Eleni <e.kiometzis@campus.tu-berlin.de>
2024-07-11 19:28:18 +0200

    Comments added and edited

--- raspi-robot/src/handleRequest.py
+++ raspi-robot/src/handleRequest.py
@@ -1,14 +1,25 @@
 import socket
 import logging
-from arduino_commands import *
 from robot_functions import Robot
 
+# Immutable command variables
+RESET = '0'
+BUCKET_ONE = '1'
+BUCKET_TWO = '2'
+GET_PACKAGE = '3'
+PACKAGE_ON_SCALE = '4'
+THRESHOLD = '5'
+UPDATED_DATABASE = '9'
 
-RASPI_IP = '0.0.0.0'
-RASPI_PORT = 2360
-ARDUINO_IP = ''
-ARDUINO_PORT = 0
-MSG_BYTES = 0
+# Error messages
+SCALE = 's'  # scale error
+WEIGHT = 'w'  # weighting error
+LIGHTBOX1 = 'l'  # light barrier error
+LIGHTBOX2 = 'L'  # light barrier error
+
+# Global variables
+ip_address = '192.168.1.105'
+
 
 class Server:
     """
@@ -18,12 +29,19 @@ class Server:
     def __init__(self, host, port) -> None:
         """
         Starts the server and initializes connection with robot.
+        :param host: Own IP address.
+        :type host: str
+        :param port: Port to listen on.
+        :type port: int
         """
         self.host = host
         self.port = port
         self.robot = Robot()
 
     def start_server(self):
+        """
+        Open a server and start listening.
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.host, self.port))
             s.listen()
@@ -38,16 +56,14 @@ class Server:
                         response = self.handle_request(data.decode('utf-8'))
                         conn.sendall(response.encode('utf-8'))
 
-    def send_message(self, message, host, port):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((host, port))
-            if not isinstance(message, bytes):
-                message = message.encode('utf-8')
-            s.sendall(message)
-            response = s.recv(1024)
-            print('Received', response.decode('utf-8'))
-
     def handle_request(self, message):
+        """
+        Decode a message and forward command to robot.
+        :param message: The message to be decoded.
+        :type message: str
+        :return: Success or error message.
+        :rtype: str
+        """
         logging.info(f"Received message: {message}")
 
         if len(message) < 5 or message[1] != '/':
@@ -85,11 +101,6 @@ class Server:
             self.robot.get_package()
             return f"OK: Sent request to get package"
 
-        # Handling error messages
-        elif command_char == MALLOC:
-            logging.error("Malloc error: failed to allocate memory for boxes array")
-            return "ERROR: Malloc error"
-
         elif command_char == SCALE:
             logging.error("Scale error: timeout, check MCU>HX711 wiring and pin designations")
             return "ERROR: Scale error"
@@ -98,40 +109,12 @@ class Server:
             logging.error("Weight error: package weights too little or too much")
             return "ERROR: Weight error"
 
-        elif command_char == LIGHT:
-            logging.error("Light barrier error: the light barrier was triggered")
-            return "ERROR: Light barrier error"
-
-        elif command_char == WIFI:
-            logging.error("WiFi error: communication with WiFi module failed")
-            return "ERROR: WiFi error"
-
-        elif command_char == TCP:
-            logging.error("TCP error: failed to connect to TCP server")
-            return "ERROR: TCP error"
-
         else:
             logging.error("Unknown command")
             return "ERROR: Unknown command"
 
 
-    """
-    send Data to arduino server
-    """
-    def sendData(self, message: str) -> None:
-        # Create a connection to the server application on port 81
-        tcp_socket = socket.create_connection(('localhost', 81))
-
-        try:
-            data = str.encode(message)
-            tcp_socket.sendall(data)
-
-        finally:
-            print("Closing socket")
-            tcp_socket.close()
-
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    server = Server('192.168.1.105', 8001)
+    logging.basicConfig(level=logging.INFO)
+    server = Server(ip_address, 8001)
     server.start_server()

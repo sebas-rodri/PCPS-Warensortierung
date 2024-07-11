19befdc1d4357d26e52e8e02de27c2e93cd5d445
Sebastian Rodriguez <r99@melao.de>
2024-07-11 17:11:21 +0200

    Added docstrings for session.py and changed default threshold
    Signed-off-by: Sebastian Rodriguez <r99@melao.de>

--- raspi-webserver/src/session.py
+++ raspi-webserver/src/session.py
@@ -3,6 +3,20 @@ import logging
 
 
 class Session:
+    """
+    Represents a session for the wareneingang application.
+
+    Attributes:
+        host (str): The host address for the session.
+        port (int): The port number for the session.
+        active (bool): Indicates whether the session is active or not.
+        box1Full (bool): Indicates whether box1 is full or not.
+        box2Full (bool): Indicates whether box2 is full or not.
+        box1 (int): The count of items in box1.
+        box2 (int): The count of items in box2.
+        threshold (int): The weight threshold in grams.
+    """
+
     def __init__(self):
         self.host = 'localhost'
         self.port = 5001
@@ -11,19 +25,39 @@ class Session:
         self.box2Full = False
         self.box1 = 0
         self.box2 = 0
-        self.threshold = 100  # in grams
+        self.threshold = 50  # in grams
     
     def start_pause(self):
+        """
+        Toggles the session between active and paused states.
+        """
         self.active = not self.active
         print(f'Active: {self.active}')
     
     def increment_box1(self):
+        """
+        Increments the count of items in box1 by 1.
+        """
         self.box1 += 1
         
     def increment_box2(self):
-       self.box2 += 1
+        """
+        Increments the count of items in box2 by 1.
+        """
+        self.box2 += 1
 
     def send_message(self, message, host, port):
+        """
+        Sends a message to the specified host and port.
+
+        Args:
+            message (str): The message to send.
+            host (str): The host address to send the message to.
+            port (int): The port number to send the message to.
+
+        Returns:
+            str: The response received from the host.
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
             if not isinstance(message, bytes):

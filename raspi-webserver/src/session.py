bb0b6037b089f4f37dd4c73ac8f203a0786ef7e5
Eleni <e.kiometzis@campus.tu-berlin.de>
2024-07-11 19:38:04 +0200

    comments added

--- raspi-webserver/src/session.py
+++ raspi-webserver/src/session.py
@@ -4,6 +4,9 @@ import logging
 
 class Session:
     def __init__(self):
+        """
+        Initializes a new Session object.
+        """
         self.host = 'localhost'
         self.port = 5001
         self.active = False
@@ -11,33 +14,39 @@ class Session:
         self.box2Full = False
         self.box1 = 0
         self.box2 = 0
-        self.threshold = 100 #in grams
-    
+        self.threshold = 100  # in grams
+
     def start_pause(self):
+        """
+        Starts or pauses current session.
+        """
         self.active = not self.active
         print(f'Active: {self.active}')
-    
-    def update_box1(self, value):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            s.sendall('4/000'.encode('utf-8'))
-            response = s.recv(1024)
-            print('Received', response.decode('utf-8'))
-    
-    def update_box2(self, value):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            s.sendall('5/000'.encode('utf-8'))
-            response = s.recv(1024)
-            print('Received', response.decode('utf-8'))
-    
+
     def increment_box1(self):
+        """
+        Increments the number of packages in first box.
+        """
         self.box1 += 1
-        
+
     def increment_box2(self):
-       self.box2 += 1
+        """
+        Increments the number of packages in second box.
+        """
+        self.box2 += 1
 
     def send_message(self, message, host, port):
+        """
+        Sends a message.
+        @param message: The message to send.
+        @type message: str
+        @param host: The IP address of the intended recipient.
+        @type host: str
+        @param port: The port the intended recipient is listening on.
+        @type port: str
+        @return: Response of the recipient.
+        @rtype: str
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
             if not isinstance(message, bytes):

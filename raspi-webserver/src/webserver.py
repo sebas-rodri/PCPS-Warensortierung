a46b70638e172f483e3665cb5eb3a60b1f0cd1ff
Eleni <e.kiometzis@campus.tu-berlin.de>
2024-07-11 18:29:12 +0200

    Comments added and little beauty changes

--- raspi-webserver/src/webserver.py
+++ raspi-webserver/src/webserver.py
@@ -5,6 +5,7 @@ import logging
 import time
 from session import Session
 import socket
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode='threading')
@@ -14,103 +15,168 @@ RESET = '0'
 BUCKET_ONE = '1'
 BUCKET_TWO = '2'
 GET_PACKAGE = '3'
+PACKAGE_ON_SCALE = '4'
 THRESHOLD = '5'
 UPDATED_DATABASE = '9'
 
 # Error messages
-MALLOC = 'm'  # malloc error
-SCALE = 's'   # scale error
+SCALE = 's'  # scale error
 WEIGHT = 'w'  # weighting error
-LIGHTBOX1 = 'l'   # light barrier error
-LIGHTBOX2 = 'L'   # light barrier error
-WIFI = 'i'    # internet error
-TCP = 't'     # server error
+LIGHTBOX1 = 'l'  # light barrier error
+LIGHTBOX2 = 'L'  # light barrier error
+
+# Global variables
+ip_address = '192.168.1.105'
 
 
 @app.route('/')
 def index():
+    """
+    Renders website.
+    @return: rendered website.
+    @rtype: object
+    """
     return render_template('index.html')
+
 
 @socketio.on('connect')
 def handle_connect():
+    """
+    Placeholder for implementation of further features on connection with client.
+    """
     print('Client connected')
+
 
 @socketio.on('get_counter_value')
 def handle_get_counter_value():
+    """
+    Saving values of counters of current session.
+    """
     print('Getting counter value')
     socketio.emit('set_counter1', {'value': activeSession.box1}, namespace='/')
     socketio.emit('set_counter2', {'value': activeSession.box2}, namespace='/')
 
+
 @socketio.on('get_threshold')
 def handle_get_threshold():
+    """
+    Saving threshold of current session.
+    """
     print('Getting threshold')
     socketio.emit('set_threshold', {'value': activeSession.threshold}, namespace='/')
 
+
 @socketio.on('get_if_full')
 def handle_get_if_full():
+    """
+    Saving status of boxes of current session.
+    """
     logging.info('Getting if full')
     if activeSession.box1Full:
         box1full()
     if activeSession.box2Full:
         box2full()
+
+
 @socketio.on('disconnect')
 def handle_disconnect():
+    """
+    Placeholder for implementation of further features on disconnection with client.
+    """
     print('Client disconnected')
 
+
 def increment(box):
+    """
+    Increment the counter of packages in the specified box.
+    @param box: ID of box to increment.
+    @type box: str
+    """
     print(f'Incrementing {box}')
     if box == 'box1':
         activeSession.increment_box1()
         print(activeSession)
-        socketio.emit('update_counter1',namespace='/', data={'value': activeSession.box1})
+        socketio.emit('update_counter1', namespace='/', data={'value': activeSession.box1})
     elif box == 'box2':
         activeSession.increment_box2()
-        socketio.emit('update_counter2', {'value': activeSession.box2},namespace='/')
+        socketio.emit('update_counter2', {'value': activeSession.box2}, namespace='/')
+
 
 def box1full():
+    """
+    Set box 1 to full.
+    """
     logging.info('Box 1 is full')
     activeSession.box1Full = True
     socketio.emit('box1_full', {'value': activeSession.box1}, namespace='/')
 
+
 def box2full():
+    """
+    Set box 2 to full.
+    """
     logging.info('Box 2 is full')
     activeSession.box2Full = True
     socketio.emit('box2_full', {'value': activeSession.box2}, namespace='/')
 
+
 @socketio.on('empty_box1')
 def box1empty():
+    """
+    Set box 1 to empty.
+    """
     logging.info('Box 1 is empty')
     activeSession.box1Full = False
     socketio.emit('enable_button', namespace='/')
     socketio.emit('box1_empty', {'value': activeSession.box1}, namespace='/')
 
+
 @socketio.on('empty_box2')
 def box2empty():
+    """
+    Set box 2 to empty.
+    """
     logging.info('Box 2 is empty')
     activeSession.box2Full = False
     socketio.emit('enable_button', namespace='/')
     socketio.emit('box2_empty', {'value': activeSession.box2}, namespace='/')
 
-@socketio.on('message_from_client')
-def handle_message(data):
-    print(f'Message from client: {data}')
-    # Process the message and optionally emit back to the client
-    socketio.emit('message_from_server', {'response': 'Message received'})
 
 @socketio.on('start/pause')
 def handle_start_pause():
+    """
+    Set a new threshold. Send fitting message to raspi-backend.
+    """
     activeSession.start_pause()
     message = '3/100'
-    activeSession.send_message(message, 'localhost', 8000)
+    activeSession.send_message(message, ip_address, 8000)
 
 
 @socketio.on('threshold')
 def update_threshold(data):
-    activeSession.threshold = int(data['theshold'])
+    """
+    Set a new threshold. Send fitting message to raspi-backend.
+    @param data: Value of threshold in gram.
+    @type data: int
+    """
+    activeSession.threshold = int(data['threshold'])
     logging.info(f'Threshold updated to {activeSession.threshold}')
-    activeSession.send_message('5/'+str(activeSession.threshold), 'localhost', 8000)
+    thresholdstr = '5/' + str(activeSession.threshold)
+    if activeSession.threshold < 10:
+        thresholdstr = '5/00' + str(activeSession.threshold)
+    elif activeSession.threshold < 100:
+        thresholdstr = '5/0' + str(activeSession.threshold)
+    activeSession.send_message(thresholdstr, ip_address, 8000)
+
 
 def handle_request(message):
+    """
+    Decode and handle incoming commands.
+    @param message: Message to be decoded.
+    @type message: str
+    @return: Success or error message.
+    @rtype: str
+    """
     logging.info(f"Received message: {message}")
 
     if len(message) < 5 or message[1] != '/':
@@ -129,26 +195,17 @@ def handle_request(message):
         return "ERROR: Invalid command or weight"
 
     if command == UPDATED_DATABASE:
-        # TODO GET NEW DATA FROM DATABASE
         logging.info(f"Received updated database: {message}")
-        # response = activeSession.send_message('get_data', 'localhost', 8000)
         if weight > activeSession.threshold:
-            
             increment('box2')
-            
+
         else:
             increment('box1')
-        # handle_get_counter_value()
-        print(weight,activeSession.threshold)
+        print(weight, activeSession.threshold)
         socketio.emit('enable_button', namespace='/')
         return "OK: Updated database"
 
-    
     # Handling error messages
-    elif command_char == MALLOC:
-        logging.error("Malloc error: failed to allocate memory for boxes array")
-        return "ERROR: Malloc error"
-
     elif command_char == SCALE:
         logging.error("Scale error: timeout, check MCU>HX711 wiring and pin designations")
         return "ERROR: Scale error"
@@ -167,24 +224,19 @@ def handle_request(message):
         box2full()
         return "ERROR: Light barrier error"
 
-    elif command_char == WIFI:
-        logging.error("WiFi error: communication with WiFi module failed")
-        return "ERROR: WiFi error"
-
-    elif command_char == TCP:
-        logging.error("TCP error: failed to connect to TCP server")
-        return "ERROR: TCP error"
-
     else:
         logging.error("Unknown command")
         return "ERROR: Unknown command"
 
 
 def start_server():
+    """
+    Open a server and start listening.
+    """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('192.168.1.105', 5001))
+        s.bind((ip_address, 5001))
         s.listen()
-        logging.info(f"Server started and listening on 192.168.1.105:5001")
+        logging.info(f"Server started and listening on {ip_address}:5001")
 
         while True:
             conn, addr = s.accept()
@@ -201,6 +253,4 @@ if __name__ == '__main__':
     activeSession = Session()
     thread = threading.Thread(target=start_server)
     thread.start()
-    socketio.run(app, debug=False, host='0.0.0.0', port=4999, allow_unsafe_werkzeug=True)
-
-
+    socketio.run(app, debug=False, host=ip_address, port=4999, allow_unsafe_werkzeug=True)
